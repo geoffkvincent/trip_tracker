6906fa5176fc73dc9d211904f49d8522b6a1a76d
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-08 11:16:07 -0700

    locaitons controller build

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,33 @@
 class LocationsController < ApplicationController
+  before_action :set_trip
+  before_action :set_location, only: [:update, :destroy]
+
   def index
+    @locations = @trip.locations
   end
 
   def create
+    location = @trip.locations.new(location_params)
+
+    if locations.save
+      render json: location
+    else
+      render json: { errors: location.errors }
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :days)
   end
 end
