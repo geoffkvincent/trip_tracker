59c3ab7f343ecde8848d4e419b68fa3d67b19a9f
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-08 11:05:25 -0700

    trip controller build

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,32 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:update, :destroy]
   def index
+    @trips = Trip.all
   end
 
   def create
+    trip = Trip.new(trip_params)
+
+    if trip.save
+      render json: trip
+    else
+      render json: { errors: trip.errors }
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name)
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:id])
   end
 end
