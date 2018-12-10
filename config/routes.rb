183304c6619dac51a1fe046964b497195747a37e
Geoff Vincent <geoffkvincent@gmail.com>
2018-12-10 09:46:05 -0700

    rails routes for locations and addresses

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :trips
+    resources :trips do
+      resources :locations
+    end
+  end
+
+  namespace :api do
+    resources :locations do
+      resources :addresses
+    end
   end
 end
