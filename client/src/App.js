634a03582c38d4d064c266f2f587dc3fec663364
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-01 21:22:49 -0700

    location props

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Trips from './components/Trips'
 import Locations from './components/Locations'
-
+import Address from './components/Address'
 
 const App = () => (
   <>
     <Switch>
       <Route exact path= '/' component={Trips} />
       <Route exact path='/trips/:trip_id/locations' component={Locations} />
+      <Route exact path='/trips/:trip_id/locations/location_id/address' component={Address} />
     </Switch>
   </>
 )
--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -6,12 +6,12 @@ class Locations extends React.Component {
 
   render() {
     const { locations } = this.props.location.state
-    const { id } = this.props.match.params
+    const { trip_id } = this.props.match.params
     return(
       <div>
         {locations.map(location =>
           <ul key={location.id}>
-            <Link to={`/trips/${id}/locations/${location.id}/address`}>
+            <Link to={`/trips/${trip_id}/locations/${location.id}/address`}>
               <li >{location.name}</li>
             </Link>
               <p>{location.days}</p>
