c983404b2f051248fdaf6508648243012a1abbd5
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-01 20:59:05 -0700

    location props

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 class Locations extends React.Component {
   render() {
+    const { locations } = this.props.location.state
     return(
-      null
+      <div>
+        {locations.map(location =>
+          <ul>
+            <li key={location.id}>{location.name}</li>
+              <p>{location.name}</p>
+          </ul>
+        )}
+      </div>
     )
   }
 }
