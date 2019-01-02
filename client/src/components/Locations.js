47094725db5dcf29a101ad7422432e90670e07db
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-01 21:14:09 -0700

    location props

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Locations extends React.Component {
+
+
   render() {
     const { locations } = this.props.location.state
+    const { url } = this.props.match
     return(
       <div>
         {locations.map(location =>
           <ul key={location.id}>
-            <li >{location.name}</li>
+            <Link to={`/url/{location.id}`}>
+              <li >{location.name}</li>
+            </Link>
               <p>{location.days}</p>
           </ul>
         )}
