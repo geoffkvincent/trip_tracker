178ecec64d7fe08987c46b2315f6b74fb44d399b
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-01 20:48:44 -0700

    location map style

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ const App = () => (
   <>
     <Switch>
       <Route exact path= '/' component={Trips} />
-      <Route path='/trips/:trip_id/locations' component={Locations} />
+      <Route exact path='/trips/:trip_id/locations' component={Locations} />
     </Switch>
   </>
 )
