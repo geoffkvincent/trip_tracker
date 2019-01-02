c52bb22ef7979dddadf73a7ba57b8d5145502062
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-01 21:25:31 -0700

    location props

---
+++ client/src/components/Address.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
+class Address extends React.Component{
+  state= { addresses: [] }
+
+  componentDidMount() {
+    axios.get()
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
+
+export default Address
